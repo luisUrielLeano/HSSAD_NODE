0b5d0ba84d78c26d53e05290981bd2bcd25b6101
luleano <luleano@icloud.com>
2021-05-31 01:26:50 -0500

    Adding the Quiz route to the router

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ import express from 'express';
 import asyncWrapper from '../utils/asyncWrapper.js';
 import userController from '../components/users/userController.js';
 import authController from '../components/auth/authController.js';
+import quizController from '../components/quiz/quizController.js';
 import isAuth from './middleware/isAuth.js';
 
 const api = express.Router();
@@ -9,10 +10,12 @@ const api = express.Router();
 //= ============== Authentication endpoints ==================
 api.route('/auth')
   .post(asyncWrapper(authController.signIn));
-
 //= ============== User management endpoints ==================
 api.route('/user')
   .get(isAuth, asyncWrapper(userController.getAllUsers))
   .post(asyncWrapper(userController.createUser));
+//= ============== Authentication endpoints ==================
+api.route('/quiz')
+  .get(asyncWrapper(quizController.getAllQuestions));
 
 export default api;
